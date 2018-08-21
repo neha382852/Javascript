59b2dae1ad3502d351df8237eab5b8f895e8ac42
Neha Anand <nanand@tavisca.com>
2018-08-21 11:44:51 +0530

    Implemented History

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 ﻿var input = "";
-
+var hist = "";
 function inputData(data) {
     input += data;
+    hist += data;
     console.log(input);
     var obj = document.getElementById("inputnum");
     obj.value = input;
@@ -9,7 +10,7 @@ function inputData(data) {
 
 function screenClear() {
     input = "";
-    inputData("");
+    document.getElementById("inputnum").value = "";
 }
 
 function calculateResult() {
@@ -17,6 +18,7 @@ function calculateResult() {
     output = document.getElementById("result");
     try {
         var result = eval(input);
+        hist = hist + "=" + result + "<br/>";
         if (isNaN(result) || (result == undefined)) {
             output.innerHTML = "Error";
         }
@@ -36,4 +38,8 @@ function keyPress(keyevent) {
     var numcode = keyevent.charCode || keyevent.KeyCode;
     if (numcode == 13)
         calculateResult();
+}
+
+function viewHistory() {
+    document.getElementById("histresult").innerHTML = hist;
 }
